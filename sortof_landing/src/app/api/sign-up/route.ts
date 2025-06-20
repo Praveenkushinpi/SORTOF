feb9e9dc364db347b8a12777d0e543212bc5abb6
Praveen kumar <168343716+Praveenkushinpi@users.noreply.github.com>
2025-06-20 10:06:34 +0530

    auth next

--- sortof_landing/src/app/api/sign-up/route.ts
+++ sortof_landing/src/app/api/sign-up/route.ts
@@ -2,6 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User"; 
 import bcrypt from "bcryptjs"
 import { sendVerificationEmail } from "@/helpers/sendVerifiactioEmail";
+import { Fahkwang } from "next/font/google";
 
 
 export async function POST(request: Request) {
@@ -33,7 +34,20 @@ export async function POST(request: Request) {
 const verifyCode = Math.floor(100000 + Math.random()*900000).toString()
 
           if (existingUserByEmail) {
-            true
+            if(existingUserByEmail.isVerified){
+                return Response.json({
+                    success: false,
+                    message: "User already exist with this email"
+                }, {status: 400})
+            } else {
+                const hasedPassword = await bcrypt.hash
+                (password, 10)
+                existingUserByEmail.password = hasedPassword;
+                existingUserByEmail.verifyCode = verifyCode;
+                existingUserByEmail.verifyCodeExpiry = new
+                Date(Date.now() + 3600000)
+                await existingUserByEmail.save()
+            }
           }else {
             const hasedPassword = await bcrypt.hash(password,
                 10
